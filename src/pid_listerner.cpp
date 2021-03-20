8e29e112ddf56e0d707d85d3178b38b0b520c950
Christopher Coballes <cobecoballes@gmail.com>
2021-03-20 18:03:26 +0800

    Create pid_listerner.cpp

---
+++ src/pid_listerner.cpp
@@ -0,0 +1,44 @@
+#include "xentrinobot_pid/pid_core.h"
+
+
+void messageCallback(const control_msgs::PidState& msg);
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "pid_listener");
+  ros::NodeHandle nh;
+
+  int rate;
+
+  ros::NodeHandle pnh("~");
+  pnh.param("rate", rate, int(40));
+
+
+  ros::Subscriber sub_message = nh.subscribe("pid", 1000,messageCallback, lino_pid);
+
+  ros::Rate r(rate);
+
+  // Main loop.
+  while (nh.ok())
+  {
+    ros::spinOnce();
+    r.sleep();
+  }
+
+  return 0;
+} // end main()
+
+void messageCallback(const control_msgs::PidState& msg)
+{
+  p_ = msg->p;
+  d_ = msg->d;
+  i_ = msg->i;
+
+  //echo P,I,D
+  ROS_INFO("P: %f", p_);
+  ROS_INFO("D: %f", d_);
+  ROS_INFO("I: %f", i_);
+}
+
+
